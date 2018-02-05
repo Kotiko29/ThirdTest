341ca38463ffbb8f2483b89d7fa99c04b5e17de8
Ekaterina Kravtsova <kotiko29@yandex.ru>
2018-02-05 20:17:58 +0500

    Третье задание

---
+++ src/test/java/ThirdTest/ThirdTest.java
@@ -0,0 +1,33 @@
+package ThirdTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.openqa.selenium.By.name;
+
+public class ThirdTest {
+        public static void main(String[] Args){
+       // System.setProperty("webdriver.chrome.driver", "C:\\Tools\\chromedriver.exe");
+      //  ChromeDriver Driver = new ChromeDriver();
+
+        FirefoxDriver Driver = new FirefoxDriver();
+        Driver.manage().window().maximize();
+        Driver.get("http://localhost/litecart/admin/");
+
+        WebElement UserNameField = Driver.findElement(By.name("username"));
+        UserNameField.clear();
+        UserNameField.sendKeys("admin");
+
+        WebElement PasswordField = Driver.findElement(By.name("password"));
+        PasswordField.clear();
+        PasswordField.sendKeys("admin");
+
+         WebElement LoginButton = Driver.findElement(By.name("login"));
+         LoginButton.click();
+
+         Driver.close();
+}
+}
+
